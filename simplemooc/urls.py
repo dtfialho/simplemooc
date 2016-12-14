19db28baf74d2a9a5f32d00d01685bf2ba42e7fa
Diego T. Fialho <diego.tfialho@gmail.com>
2016-12-14 15:41:19 -0200

    Init

---
+++ simplemooc/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'simplemooc.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'simplemooc.core.views.home', name='home'),
+)
