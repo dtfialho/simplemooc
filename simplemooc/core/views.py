49165492d3ac612a1f26f189a28856d9684fc955
Diego T. Fialho <diego.tfialho@gmail.com>
2016-12-14 16:37:27 -0200

    First template

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('Hello World')
+	context = {
+		"usuario": "Diego"
+	}
+
+	return render(request, 'home.html', context)
