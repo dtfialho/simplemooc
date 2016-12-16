e20815156774b7464e59b0947e949407e57c0fea
Diego T. Fialho <diego.tfialho@gmail.com>
2016-12-16 10:34:36 -0200

    Urls added

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -4,11 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'simplemooc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('simplemooc.core.urls', namespace="core")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'simplemooc.core.views.home', name='home'),
-    url(r'^contato/$', 'simplemooc.core.views.contact', name='contact'),
 )
